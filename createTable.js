6a7b3b5bcfca2139cc5787e5c9891a04ca3a4dce
wgk <2287334379@qq.com>
2025-06-15 02:34:38 +0800

    1

--- createTable.js
+++ createTable.js
@@ -2,9 +2,10 @@
 import fs from 'fs';
 import yaml from 'js-yaml'
 import util from './util.js';
-const config = yaml.load(fs.readFileSync('C:/Users/22873/Desktop/config.yaml', 'utf8'));
+const config = yaml.load(fs.readFileSync('D:/auto/config.yaml', 'utf8'));
 
 
 //创建rbac数据库表
-util.createDbAndTable(config);
+await util.createDbAndTable(config);
+//拉取前后端代码
 
