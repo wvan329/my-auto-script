731903bdd89839f5cb26f63d172497bc544db57f
wgk <2287334379@qq.com>
2025-07-15 22:50:19 +0800

    1

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 import fs from 'fs-extra';
 import { app } from './config.js';
-import { name_port } from './name.js';
+import { name } from './name.js';
 
 const createDbAndTable = async () => {
   const connection = await mysql.createConnection({
@@ -11,8 +11,8 @@ const createDbAndTable = async () => {
     multipleStatements: true,
   });
 
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${name_port}\`;`);
-  await connection.query(`USE \`${name_port}\`;`);
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${name}\`;`);
+  await connection.query(`USE \`${name}\`;`);
   const script = fs.readFileSync('D:/auto/config/sql.txt', 'utf8');
   await connection.query(script);
   await connection.end();
