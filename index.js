4da54f966266183b90502878858c5346e65062ed
wgk <2287334379@qq.com>
2025-08-29 22:22:29 +0800

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { execFile } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const args = process.argv.slice(2);
+
+if (args[0] === "install") {
+  console.log(install);
+} else if (args[0] === "uninstall") {
+  console.log(install);
+}
