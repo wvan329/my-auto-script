491f6ccdbd7b634ecbd1e93e189e7093f6f31bdc
wgk <2287334379@qq.com>
2025-08-29 22:29:30 +0800

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { install } from "./install";
-import { uninstall } from "./uninstall";
+import { install } from "./install.js";
+import { uninstall } from "./uninstall.js";
 const args = process.argv.slice(2);
 
 if (args[0] === "install") {
